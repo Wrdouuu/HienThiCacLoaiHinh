5c712a49fca63bdc9d889148f1f641db287bc62b
Wrdouuu <164611150+Wrdouuu@users.noreply.github.com>
2024-08-16 19:06:22 +0700

    s

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HIENTHICACLOAIHINH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1; i<=5; i++)
+            {
+                for(int j=5; j>=i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+             for (int i = 1; i <= 3; i++)
+             {
+                for (int j = 1; j <= 7; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+             }
+        }
+    }
+}
